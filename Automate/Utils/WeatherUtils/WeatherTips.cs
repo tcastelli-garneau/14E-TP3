d5dabd5c2643a855b7d11f0b84b5ad31d17604fa
Thibault Castelli <thcas004@edu.cegepgarneau.ca>
2024-11-25 14:38:09 -0500

    Add GetHumidityTips

--- Automate/Utils/WeatherUtils/WeatherTips.cs
+++ Automate/Utils/WeatherUtils/WeatherTips.cs
@@ -15,24 +15,59 @@
         public string GetTemperatureTips(bool isHeatingOn, bool isWindowOpen, int currentTemperature)
         {
             if (isHeatingOn && !isWindowOpen && currentTemperature > MAX_TEMPERATURE_GOAL)
-                return "La température de la serre est trop élevée. Veuillez éteindre le chauffage et/ou ouvrir les fenêtres";
+                return "La température de la serre est trop élevée. Veuillez éteindre le chauffage et/ou ouvrir les fenêtres.";
 
             if (isHeatingOn && isWindowOpen && currentTemperature > MAX_TEMPERATURE_GOAL)
-                return "La température de la serre est trop élevée. Veuillez éteindre le chauffage";
+                return "La température de la serre est trop élevée. Veuillez éteindre le chauffage.";
 
             if (!isHeatingOn && !isWindowOpen && currentTemperature > MAX_TEMPERATURE_GOAL)
-                return "La température de la serre est trop élevée. Veuillez ouvrir les fenêtres";
+                return "La température de la serre est trop élevée. Veuillez ouvrir les fenêtres.";
 
             if (!isHeatingOn && isWindowOpen && currentTemperature < MIN_TEMPERATURE_GOAL)
-                return "La température de la serre est trop basse. Veuillez allumer le chauffage et/ou fermer les fenêtres";
+                return "La température de la serre est trop basse. Veuillez allumer le chauffage et/ou fermer les fenêtres.";
 
             if (!isHeatingOn && !isWindowOpen && currentTemperature < MIN_TEMPERATURE_GOAL)
-                return "La température de la serre est trop basse. Veuillez allumer le chauffage";
+                return "La température de la serre est trop basse. Veuillez allumer le chauffage.";
 
             if (isHeatingOn && isWindowOpen && currentTemperature < MIN_TEMPERATURE_GOAL)
-                return "La température de la serre est trop basse. Veuillez fermer les fenêtres";
+                return "La température de la serre est trop basse. Veuillez fermer les fenêtres.";
 
-            return "";
+            if (currentTemperature > MAX_TEMPERATURE_GOAL)
+                return "La température de la serre est trop élevée. Aucune action supplémentaire recommandée.";
+
+            if (currentTemperature < MIN_TEMPERATURE_GOAL)
+                return "La température de la serre est trop basse. Aucune action supplémentaire recommandée.";
+
+            return "La température de la serre est correcte.";
+        }
+
+        public string GetHumidityTips(bool isWateringOn, bool isVentilationOn, int currentHumidity)
+        {
+            if (isWateringOn && !isVentilationOn && currentHumidity > MAX_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop élevé. Veuillez désactiver le système d'arrosage et activer le système de ventilation.";
+
+            if (isWateringOn && isVentilationOn && currentHumidity > MAX_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop élevé. Veuillez désactiver le système d'arrosage.";
+
+            if (!isWateringOn && !isVentilationOn && currentHumidity > MAX_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop élevé. Veuillez activer le système de ventilation.";
+
+            if (!isWateringOn && isVentilationOn && currentHumidity < MIN_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop bas. Veuillez activer le système d'arrosage et désactiver le système de ventilation.";
+
+            if (!isWateringOn && !isVentilationOn && currentHumidity < MIN_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop bas. Veuillez activer le système d'arrosage.";
+
+            if (isWateringOn && isVentilationOn && currentHumidity < MIN_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop bas. Veuillez désactiver le système de ventilation.";
+
+            if (currentHumidity > MAX_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop élevé. Aucune action supplémentaire recommandée.";
+
+            if (currentHumidity < MAX_HUMIDITY_GOAL)
+                return "Le taux d'humidité est trop bas. Aucune action supplémentaire recommandée.";
+
+            return "Le taux d'humidité est correct.";
         }
     }
 }
